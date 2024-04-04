ea6142b8ec5251bc88eaadda7670720af908653e
sidkotapati <143844440+sidkotapati@users.noreply.github.com>
2024-04-04 17:48:19 -0400

    Update mbot_robot.py

--- mbot_robot.py
+++ mbot_robot.py
@@ -17,10 +17,10 @@ class Robot:
             
     def drive(self, speed, time):
         SAMPLETIME = 0.1
-        TARGET = 400*speed
-        KP = 0.0009
-        KD = 0.00005
-        KI = 0.00001
+        TARGET = 200*speed
+        KP = 0.002
+        KD = 0.0001
+        KI = 0.00005
         e0_prev_error = 0
         e1_prev_error = 0
         
@@ -35,35 +35,27 @@ class Robot:
         self.mot0.set(m0_speed)
         self.mot1.set(m1_speed)
         print("m0 {} m1 {}".format(m0_speed, m1_speed))
-        self.delay(0.02)
+        utime.sleep_ms(500)
+        print("e0 {} e1 {}".format(self.enc0.encoderCount, self.enc1.encoderCount))
         
         for _ in range(int(time*(1/SAMPLETIME))):
             e0_error = TARGET - self.enc0.encoderCount
             e1_error = TARGET - self.enc1.encoderCount
-            print("e0 {} e1 {}".format(self.enc0.encoderCount, self.enc1.encoderCount))
             print("err0 {} err1 {}".format(e0_error, e1_error))
             m0_speed += (e0_error * KP) + (e0_prev_error * KD) + (e0_sum_error * KI)
             m1_speed += (e1_error * KP) + (e1_prev_error * KD) + (e1_sum_error * KI)
 
             print("m0 {} m1 {}".format(m0_speed, m1_speed))
             
-            if (m0_speed > 1 or m0_speed < -1):
-                print("m0 speed out of bounds")
-                self.mot0.set(0)
-                self.mot1.set(0)
-                break
-            if (m1_speed > 1 or m1_speed < -1):
-                print("m1 speed out of bounds")
-                self.mot0.set(0)
-                self.mot1.set(0)
-                break
+            m0_speed = max(min(1, m0_speed), -1)
+            m1_speed = max(min(1, m1_speed), -1)
             
             print("m0 {} m1 {}".format(m0_speed, m1_speed))
             print("e0 {} e1 {}".format(self.enc0.encoderCount, self.enc1.encoderCount))
 
             self.mot0.set(m0_speed)
             self.mot1.set(m1_speed)
-
+            
             self.enc0.reset()
             self.enc1.reset()
             
@@ -74,43 +66,32 @@ class Robot:
             e1_sum_error += e1_error
             
             self.delay(SAMPLETIME)
+            
 
         self.mot0.set(0)
         self.mot1.set(0)
         print(TARGET)
-        self.delay(0.02)
+        utime.sleep_ms(20)
         
     def turnleft(self):
-        #need to implement using control to make
-        #sure you get the right number of encoder clicks
-        self.enc1.reset()
-        while((self.enc1.encoderCount) < 1300):
-            self.mot0.set(-0.1)
-            self.mot1.set(0.3)
-            self.delay(0.01)
-        
-        self.mot1.set(0)
+        utime.sleep_ms(50)
+        self.mot0.set(0.4)
+        self.mot1.set(0.4)
+        utime.sleep_ms(520)
         self.mot0.set(0)
-        self.delay(0.01)
-        
+        self.mot1.set(0)
+        utime.sleep_ms(50)
+
     def turnright(self):
-        #need to implement using control to make
-        #sure you get the right number of encoder clicks
-        self.enc0.reset()
-        while((self.enc0.encoderCount) < 1300):
-            self.mot1.set(-0.1)
-            self.mot0.set(0.3)
-            self.delay(0.01)
-            
-        self.mot1.set(0)
+        utime.sleep_ms(50)
+        self.mot0.set(-0.4)
+        self.mot1.set(-0.4)
+        utime.sleep_ms(520)
         self.mot0.set(0)
-        self.delay(0.01)
-        
+        self.mot1.set(0)
+        utime.sleep_ms(50)
         
 if __name__ == "__main__":
     r = Robot()
     r.delay(3)
     r.drive(0.6, 10)
-    #for i in range(10):
-        #r.turnright()
-        #r.turnleft()
