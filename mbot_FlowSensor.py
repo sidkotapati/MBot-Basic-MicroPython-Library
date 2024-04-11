931d5d0eb3d344dd364a4b116b7faafb2ebf2487
gabstav <143836787+gabstav@users.noreply.github.com>
2024-04-11 13:05:23 -0400

    Update mbot_FlowSensor.py

--- mbot_FlowSensor.py
+++ mbot_FlowSensor.py
@@ -1,27 +1,66 @@
 import time
-
-# Pick *one* sensor type by uncommenting the relevant line below:
-
-# PMW3901
 from breakout_pmw3901 import BreakoutPMW3901 as FlowSensor
 
-# PAA5100
-# from breakout_paa5100 import BreakoutPAA5100 as FlowSensor
 
-flo = FlowSensor()
-flo.set_rotation(FlowSensor.DEGREES_0)
+class PID:
+    
+    integral = 0
+    error_old = 0
+    error = 0
+    
+    def __init__(self, kp, ki, kd, target):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        
+        self.target = target
+        
+    def set_target(self, target):
+        self.target = target
+    
+    def control(self, current):
+        self.error_old = self.error
+        
+        self.error = self.target - current
+        self.integral += self.error*dt
+        differential = (self.error - self.error_old) / dt
+        
+        output = self.kp*self.error + self.ki*self.integral + self.kd*differential
+        
+        return output
 
-tx = 0
-ty = 0
-x = 0
-y = 0
 
-while True:
-    delta = flo.get_motion()
-    if delta is not None:
-        x = delta[0]
-        y = delta[1]
-        tx += x
-        ty += y
-        print("Relative: x {}, y {} | Absolute: tx {}, ty {}".format(x, y, tx, ty))
-    time.sleep(0.1)
+class flowsensor:
+    
+    flo = Flowsensor()
+    flo.set_rotation(FlowSensor.DEGREES_0)
+    
+    straight = PID(0.1, 0, 0, 0)
+    speed = PID(0.1, 0, 0, 0)
+    
+    def get_xy(self):
+        # returns a vector (x, y) of the flowsensor
+        return self.flo.get_motion()
+    
+    def print_xy(self):
+        delta = self.flo.get_motion()
+        print("Relative: x {}, y {}".format(delta[0], delta[1]))
+    
+    def straight_control(self):
+        return self.straight.control(self.get_xy()[0])
+    
+    def set_speed(self, speed):
+        self.speed.set_target(speed)
+    
+    def speed_control(self):
+        return self.speed.control(self.get_xy()[1])
+
+
+def stop(self, prev_speed):
+    change = prev_speed / 5
+    for _ in range(5):
+        self.mot0.set(prev_speed)
+        self.mot1.set(prev_speed)
+        prev_speed -= change
+        self.delay(0.1)
+    
