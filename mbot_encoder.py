5360ff7783dea95a960d7c6aa4f635e70c6c1dd6
sidkotapati <143844440+sidkotapati@users.noreply.github.com>
2024-04-19 12:13:02 -0400

    Update mbot_encoder.py
    converted the encoders classes into 1 merged class with an input for motor/encoder orientation.

--- mbot_encoder.py
+++ mbot_encoder.py
@@ -1,13 +1,16 @@
 from machine import Pin
 import utime
 
-class Encoder0:
-    def __init__(self, pinA, pinB):
+#1  wheel revolution is equal to 3142 clicks (pi*1000)
+class Encoder:
+    # orientation should be a boolean value
+    def __init__(self, pinA, pinB, orientation):
         self.encoderCount = 0
         self.A = Pin(pinA, Pin.IN, Pin.PULL_UP)
         self.B = Pin(pinB, Pin.IN, Pin.PULL_UP)
         self.A.irq(self.A_ISR, Pin.IRQ_RISING or Pin.IRQ_FALLING)
         self.B.irq(self.B_ISR, Pin.IRQ_RISING or Pin.IRQ_FALLING)
+        self.orient = orientation
 
     def A_ISR(self, pin):
         if self.A.value():
@@ -34,60 +37,17 @@ class Encoder0:
                 self.dec()
     
     def inc(self):
-        self.encoderCount += 1
+        self.encoderCount += 4*(1+(self.orient*-2))
         
     def dec(self):
-        self.encoderCount -= 1
+        self.encoderCount -= 4*(1+(self.orient*-2))
         
     def reset(self):
         self.encoderCount = 0
-        
-        
-class Encoder1:
-    def __init__(self, pinA, pinB):
-        self.encoderCount = 0
-        self.A = Pin(pinA, Pin.IN, Pin.PULL_UP)
-        self.B = Pin(pinB, Pin.IN, Pin.PULL_UP)
-        self.A.irq(self.A_ISR, Pin.IRQ_RISING or Pin.IRQ_FALLING)
-        self.B.irq(self.B_ISR, Pin.IRQ_RISING or Pin.IRQ_FALLING)
-
-    def A_ISR(self, pin):
-        if self.A.value():
-            if self.B.value():
-                self.dec()
-            else:
-                self.inc()
-        else:
-            if self.B.value():
-                self.inc()
-            else:
-                self.dec()
-    
-    def B_ISR(self, pin):
-        if self.B.value():
-            if self.A.value():
-                self.inc()
-            else:
-                self.dec()
-        else:
-            if self.A.value():
-                self.dec()
-            else:
-                self.inc()
-    
-    def inc(self):
-        self.encoderCount += 1
-        
-    def dec(self):
-        self.encoderCount -= 1
-        
-    def reset(self):
-        self.encoderCount = 0
-
             
 if __name__ == "__main__":
-    enc0 = Encoder0(6, 7)
-    enc1 = Encoder1(8, 9)
+    enc0 = Encoder(6, 7, 0)
+    enc1 = Encoder(8, 9, 1)
 
     while True:
         print(f"ENC: {enc0.encoderCount} | {enc1.encoderCount}\r")
